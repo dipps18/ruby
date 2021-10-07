eefb19c0d086977e1495538c5cfe197bb8ff96fe
dipsthedestroya <dipupurecha01@gmail.com>
2021-10-07 09:04:59 +0400

    cleaned code

--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -2,10 +2,9 @@ def substrings(string,dictionary)
     result = Hash.new(0)
     string.downcase!
     
-    dictionary.map do |element|
+    dictionary.filter_map do |element|
         element.downcase!
         result[element]=(string.scan(element).length) if(string.include?(element))
-        result[element] unless result[element]==nil
     end
     result
 end
