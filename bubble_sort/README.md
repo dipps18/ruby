15fa1049fe42a681a98bccbf4110f66b8af6af31
dipsthedestroya <dipupurecha01@gmail.com>
2021-10-08 14:57:51 +0400

    Cleaned code

--- bubble_sort/README.md
+++ bubble_sort/README.md
@@ -1,5 +1,5 @@
 
     Build a method #bubble_sort that takes an array and returns a sorted array. It must use the bubble sort methodology
 
-> bubble_sort([4,3,78,2,0,2])
+bubble_sort([4,3,78,2,0,2])
 => [0,2,2,3,4,78]
--- substrings/substrings.rb
+++ substrings/substrings.rb
@@ -1,15 +1,16 @@
-def substrings(string,dictionary)
-    result = Hash.new(0)
-    string_lower=string.downcase
-    
-    dictionary.filter_map do |element|
-        element_lower=element.downcase
-        result[element_lower]=(string_lower.scan(element_lower).length) if(string_lower.include?(element_lower))
-    end
-    result
+# frozen_string_literal: true
+
+def substrings(string, dictionary)
+  result = Hash.new(0)
+  string_lower = string.downcase
+
+  dictionary.filter_map do |element|
+    element_lower = element.downcase
+    result[element_lower] = string_lower.scan(element_lower).length if string_lower.include?(element_lower)
+  end
+  result
 end
 
-dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("below", dictionary)
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+substrings('below', dictionary)
 substrings("Howdy partner, sit down! How's it going?", dictionary)
-            
